9d4031988ad70e21ffba3a5c04d5037a7af70814
DigiMax <42356489+DigiMaxIO@users.noreply.github.com>
2024-07-31 12:13:20 -0700

    Update absolutePosition.ino

--- examples/absolute-position/absolutePosition.ino
+++ examples/absolute-position/absolutePosition.ino
@@ -1,11 +1,10 @@
 #include <Arduino.h>
 #include <IQS7211A.h>
 
-
 /*** Defines ***/
-#define DEMO_IQS7211A_ADDR        0x56
-#define DEMO_IQS7211A_POWER_PIN   2
-#define DEMO_IQS7211A_RDY_PIN     A3
+#define IQS7211A_ADDR     0x56
+#define IQS_RST_PIN          2
+#define IQS_RDY_PIN         A3
 
 /*** Instances ***/
 IQS7211A trackpad;
@@ -13,26 +12,21 @@ IQS7211A trackpad;
 /*** Global Variables ***/
 bool show_data = false;
 iqs7211a_power_modes running_power_mode = IQS7211A_IDLE;
-uint16_t running_x_output = 65535;
-uint16_t running_y_output = 65535;
 iqs7211a_gestures_e running_gestures = IQS7211A_GESTURE_NONE;
 
 bool printData(void);
-void printCoordinates(void);
-void init_iqs7211a(void);
+void printAbsCoordinates(void);
 void init_serial_comms(void);
 
 void setup()
 {
   init_serial_comms();
-  init_iqs7211a();
+  trackpad.begin(IQS7211A_ADDR, IQS_RDY_PIN, IQS_RST_PIN);
 }
 
 void loop()
 {
-  /* Read new data from IQS7211A if available  (RDY Line Low) */
   trackpad.run();
-
   /* If data was updated, display data read from IQS7211A */
   if(trackpad.new_data_available)
   {
@@ -53,7 +47,7 @@ void loop()
 in the serial terminal */
 bool printData(void)
 {
-/* See if it is necessary to display the button state, power mode or gestures */
+  /* See if it is necessary to display the button state, power mode or gestures */
   if(trackpad.touchpad_event_occurred())
   {
     return true; // Let the main loop know to display the latest values
@@ -62,12 +56,11 @@ bool printData(void)
   {
     return false; // Let the main loop know to not display the latest values
   }
-
   return false;
 }
 
 /* Function to return X and Y coordinates of finger 1 */
-void printCoordinates(void)
+void printAbsCoordinates(void)
 {
   uint16_t ui16TempX = trackpad.getAbsXCoordinate(FINGER_1);
   uint16_t ui16TempY = trackpad.getAbsYCoordinate(FINGER_1);
@@ -76,21 +69,6 @@ void printCoordinates(void)
   Serial.print("\t\t");    // Spacing to match headings in Print
   Serial.print(ui16TempY); // Print Y coordinates
   Serial.print("\t\t");    // Spacing to match headings in Print
-  running_x_output = ui16TempX;
-  running_y_output = ui16TempY;
-}
-
-void init_iqs7211a(void)
-{
-  pinMode(DEMO_IQS7211A_POWER_PIN, OUTPUT);
-  delay(200);
-  digitalWrite(DEMO_IQS7211A_POWER_PIN, LOW);
-  delay(200);
-  digitalWrite(DEMO_IQS7211A_POWER_PIN, HIGH);
-  Serial.println("IQS7211A MikroE Touchpad4 Selected!");
-  trackpad.begin(DEMO_IQS7211A_ADDR, DEMO_IQS7211A_RDY_PIN);
-  Serial.println("IQS7211A Ready");
-  delay(1);
 }
 
 void init_serial_comms(void)
